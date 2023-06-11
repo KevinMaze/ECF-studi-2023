34289e83a37b760083025b0bd1fb10809efee128
Kévin MAZE <kekeproject@hotmail.fr>
2023-06-11 18:09:31 +0200

    add gitignore, script and style files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="*">
+        <link rel="stylesheet" href="style.css">
+        <title>Garage V.Parrot (ECF hiver 2023)</title>
+        <link rel="icon" type="image/png" href="/assets/iconVoiture.png">
+    </head>
+    <body>
+
+        <header>
+            <h1>Bienvenue</h1>
+
+        </header>
+
+
+        <footer>
+
+        </footer>
+
+        <script src="script.js"></script>
+    </body>
+</html>
