3a65e88e17d3a0f7682a27bb226f8830661d01d6
Kévin MAZE <kekeproject@hotmail.fr>
2023-06-11 18:42:31 +0200

    add index.php, style.css, reset.css and header.php

--- index.php
+++ index.php
@@ -4,16 +4,42 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="*">
-        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/style.css">
         <title>Garage V.Parrot (ECF hiver 2023)</title>
         <link rel="icon" type="image/png" href="/assets/iconVoiture.png">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     </head>
     <body>
 
         <header>
-            <h1>Bienvenue</h1>
-
+            <nav>
+                <img src="/assets/logo.png" alt="logo du garage" width=100>
+                <a href="#">Accueil</a>
+                <a href="#">Voitures Occasions</a>
+                <a href="#">Services</a>
+                <button type="button" class="btn btn-primary">Connection</button>
+            </nav>
         </header>
+
+        <section>
+            <div class="flux">
+                <h1>Garage Vincent Parrot</h1>
+
+                <p>Vincent Parrot, fort de ses 15 années d'expérience dans la réparation automobile, a ouvert 
+                son propre garage à Toulouse en 2021.<br>
+                Depuis 2 ans, il propose une large gamme de services: réparation de la carrosserie et de la 
+                mécanique des voitures ainsi que leur entretien régulier pour garantir leur performance et 
+                leur sécurité. De plus, le Garage V. Parrot met en vente des véhicules d'occasion.<br>
+                Vincent Parrot considère son atelier comme un véritable lieu de confiance pour ses clients et 
+                leurs voitures doivent, selon lui, à tout prix être entre de bonnes mains. 
+                </p> 
+            </div>
+
+        </section>
 
 
         <footer>
@@ -21,5 +47,6 @@
         </footer>
 
         <script src="script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </body>
 </html>
