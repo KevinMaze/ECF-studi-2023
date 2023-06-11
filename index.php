607e62dabcc2391bdf62aac40e7c9ca815ff21ef
Kévin MAZE <kekeproject@hotmail.fr>
2023-06-11 19:17:59 +0200

    split footer end header php

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="*">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/reset.css">
-        <link rel="stylesheet" href="/css/style.css">
-        <title>Garage V.Parrot (ECF hiver 2023)</title>
-        <link rel="icon" type="image/png" href="/assets/iconVoiture.png">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    </head>
-    <body>
-
-        <header>
-            <nav>
-                <img src="/assets/logo.png" alt="logo du garage" width=100>
-                <a href="#">Accueil</a>
-                <a href="#">Voitures Occasions</a>
-                <a href="#">Services</a>
-                <button type="button" class="btn btn-primary">Connection</button>
-            </nav>
-        </header>
+<?php require_once "templates/header.html" ?>
 
         <section>
-            <div class="flux">
+            <div class="flux section">
                 <h1>Garage Vincent Parrot</h1>
 
                 <p>Vincent Parrot, fort de ses 15 années d'expérience dans la réparation automobile, a ouvert 
@@ -41,12 +16,7 @@
 
         </section>
 
+         <!-- voir accordion hover -->
 
-        <footer>
 
-        </footer>
-
-        <script src="script.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    </body>
-</html>
+<?php require_once "templates/footer.html" ?>
