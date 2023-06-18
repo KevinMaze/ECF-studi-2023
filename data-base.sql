4387c1c1eef3736bd6ce2b58d944dea23c31a376
KevinMaze <kekeprojectphotmail.fr>
2023-06-18 13:16:50 +0200

    delet doc

--- data-base.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE VincentParrotCars
-
-CREATE TABLE cars (
-  'id' int(11) not null AUTO_INCREMENT,
-  'Name' varchar(50) NOT NULL,
-  'Price' int(10000) NOT NULL,
-  'image' varchar(100) DEFAULT NULL,
-  'mileage' int(100000) NOT NULL
-)
