3b3b090a181976a210458f73a855a1c63773eddc
KevinMaze <kekeprojectphotmail.fr>
2023-06-21 14:49:22 +0200

    add img service

--- index.php
+++ index.php
@@ -20,53 +20,45 @@ require_once ("lib/car.php");
             </div>
 
         </section class="flux">
-            <div class="section__div backg-opacity">
+            <div class="section__div flux backg-opacity">
                 <h2>Nos services</h2>
-                <div class="container text-center">
-                    <div class="row">
-                        <div class="col-lg">
+                <div class="text-center">
+                    <div class="row justify-content-center">
+                        <div class="col-md-3">
                             <img src="assets/vidange.jpg" alt="vidange" class="section__service__img">
                             <p class="section__service__p">Révision et vidange</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
+                        <div class="col-md-3">
+                            <img src="assets/pneu.jpg" alt="pneu" class="section__service__img">
                             <p class="section__service__p">Pneus</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/frein.jpg" alt="vidange" class="section__service__img">
+                        <div class="col-md-3">
+                            <img src="assets/frein.jpg" alt="frein" class="section__service__img">
                             <p class="section__service__p">Freinage</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
+                        <div class="col-md-3">
+                            <img src="assets/courroie.jpg" alt="distribution" class="section__service__img">
+                            <p class="section__service__p">Distribution</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
+                        <div class="col-md-3">
+                            <img src="assets/amortisseurs.jpg" alt="amortisseurs" class="section__service__img">
+                            <p class="section__service__p">Amortisseurs</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
+                        <div class="col-md-3">
+                            <img src="assets/batterie.jpg" alt="batterie" class="section__service__img">
+                            <p class="section__service__p">Batterie et Eclairage</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
+                        <div class="col-md-3">
+                            <img src="assets/climatisation.jpg" alt="climatisation" class="section__service__img">
+                            <p class="section__service__p">Climatisation</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
+                        <div class="col-md-3">
+                            <img src="assets/bougie.jpg" alt="bougies" class="section__service__img">
+                            <p class="section__service__p">Bougies</p>
                         </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
-                        </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
-                        </div>
-                        <div class="col-lg">
-                            <img src="assets/pneu.jpg" alt="vidange" class="section__service__img">
-                            <p class="section__service__p">Pneus</p>
+                        <div class="col-md-3">
+                            <img src="assets/entretiens.jpg" alt="entretiens" class="section__service__img">
+                            <p class="section__service__p">Mécanique, entretiens, contrôle</p>
                         </div>
 
                     </div>
@@ -77,7 +69,7 @@ require_once ("lib/car.php");
         </section>
 
 
-        <section class="carrousel flux">
+        <section class="carrousel flux backg-opacity">
 
             <?php foreach ($cars as $key => $car) {
 
